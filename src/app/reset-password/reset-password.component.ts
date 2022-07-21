ae23851b7d1fe5ad55d2c895c414a950700d8855
Vincent R <98562523+vjv6913@users.noreply.github.com>
2022-07-21 18:24:22 -0400

    Fornt end for reset PW and add button to nav. Also made Logo link to home

---
+++ src/app/reset-password/reset-password.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ResetPasswordService } from '../reset-password.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.css']
+})
+export class ResetPasswordComponent implements OnInit {
+
+  editPassword! : FormGroup
+
+
+
+
+  constructor(
+    private fb: FormBuilder,
+    private resetPasswordService: ResetPasswordService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {}
+
+  ngOnInit(): void {
+    this.editPassword = this.fb.group({
+      email   : [],
+      currPW  : [],
+      newPW   : [],
+      reNewPW : []
+    })
+  }
+
+  resetPW() {
+    if (this.editPassword.valid) {
+      let itemModel = this.editPassword.value
+      console.log(itemModel)      
+      this.resetPasswordService.savePassword(itemModel)
+      .subscribe({
+        next: (response: any) => {
+          console.log(response)
+        }
+      })
+      this.router.navigate(['']);
+  }
+
+}
+
+}
